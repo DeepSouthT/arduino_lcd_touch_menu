d670af870b2f58eef082d313b84e319a9db7720a
deepsoutht <deepakcheriyan@yahoo.com>
2020-06-04 22:30:49 +0200

    minor improvements adding settings icon Updated icons Delay for button

--- arduino_lcd_touch_menu.ino
+++ arduino_lcd_touch_menu.ino
@@ -35,12 +35,13 @@
  *     0.1 > Basic menu with four screens (no image)
  *     0.2 > Basic menu with four screens with image
  *     
- * Last modified: 19.10.2019
+ * Last modified: 10.11.2019
  *******************************/
 
 #include <UTFT.h>
 #include <URTouch.h>
-#include <MemoryFree.h>
+
+#include "statusbar.h"
 
 // Initialize display
 // ------------------
@@ -51,6 +52,8 @@ UTFT myGLCD(ITDB32S_V2, 38, 39, 40, 41);
 // ----------------------
 URTouch  myTouch( 6, 5, 4, 3, 2);
 
+statusbar statusbar_obj(myGLCD);
+
 // Declare which fonts we will be using
 extern uint8_t BigFont[];
 extern uint8_t SevenSegNumFont[];
@@ -59,30 +62,47 @@ extern uint8_t SevenSegNumFont[];
 int x_touch, y_touch;
 
 // Icon
-extern unsigned int arrow_left[0x834];
-extern unsigned int arrow_right[0x834];
-extern unsigned int home_icon[0x1254];
+extern unsigned int arrowHeadR[0x23F];
+extern unsigned int arrowHeadL[0x23F];
+extern unsigned int house[0x866];
 extern unsigned int temp_icon[0x1254];
-extern unsigned int clock_icon[0x1210];
-extern unsigned int currency_icon[0x1254];
+extern unsigned int clockNeedle[0x9C4];
+extern unsigned int doller[0x834];
 extern unsigned int semicolon[0x172];
 extern unsigned int centigrade[0x60C];
 extern unsigned int german[0xB6C];
 extern unsigned int canada[0xB41];
+extern unsigned int settings[0xAF9];
 
 /*************************
 **   Custom functions   **
 *************************/
+
+// Function for drawing initial static areas
+void initStaticArea(void)
+{
+  statusbar_obj.drawStatusbar();
+
+  myGLCD.setColor(VGA_BLACK);
+  myGLCD.fillRect (0, 31, 319, 239);
+}
+
+// Function for clearing the complete menu area
+void clearMenuArea(void)
+{
+  myGLCD.setColor(VGA_BLACK);
+  myGLCD.fillRect (0, 31, 319, 179);
+}
 
 // Function for drawing right button
 void rightButton()
 {
   myGLCD.setColor(255, 255, 255);
   myGLCD.drawRoundRect (228, 180, 310, 230);
-  myGLCD.drawBitmap(246, 185, 50, 42, arrow_right);
+  myGLCD.drawBitmap(265, 194, 25, 23, arrowHeadR);
 
-  Serial.println("rightButton done");
-  Serial.println(freeMemory());
+  myGLCD.setColor(10, 116, 255);
+  myGLCD.fillRect (240, 200, 265, 210);
 }
 
 // Function for drawing left button
@@ -90,10 +110,10 @@ void leftButton()
 {
   myGLCD.setColor(255, 255, 255);
   myGLCD.drawRoundRect (10, 180, 92, 230);
-  myGLCD.drawBitmap(25, 185, 50, 42, arrow_left);
+  myGLCD.drawBitmap(25, 194, 25, 23, arrowHeadL);
 
-  Serial.println("leftButton done");
-  Serial.println(freeMemory());
+  myGLCD.setColor(10, 116, 255);
+  myGLCD.fillRect (50, 200, 75, 210);
 }
 
 // Function for drawing red button outline during the butten is pressed
@@ -101,15 +121,13 @@ void rightRedButton()
 {
   myGLCD.setColor(255, 0, 0);
   myGLCD.drawRoundRect (228, 180, 310, 230);
+  delay(150);
   while (myTouch.dataAvailable())
   {
     myTouch.read();
   }
   myGLCD.setColor(255, 255, 255);
   myGLCD.drawRoundRect (228, 180, 310, 230);
-
-  Serial.println("rightRedButton done");
-  Serial.println(freeMemory());
 }
 
 // Function for drawing red button outline during the butten is pressed
@@ -117,30 +135,30 @@ void leftRedButton()
 {
   myGLCD.setColor(255, 0, 0);
   myGLCD.drawRoundRect (10, 180, 92, 230);
+  delay(150);
   while (myTouch.dataAvailable())
   {
     myTouch.read();
   }
   myGLCD.setColor(255, 255, 255);
   myGLCD.drawRoundRect (10, 180, 92, 230);
+}
 
-  Serial.println("leftRedButton done");
-  Serial.println(freeMemory());
+// Function for drawing settings button
+void settingsButton()
+{
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.drawRoundRect (165, 180, 267, 230);
+  myGLCD.drawBitmap(195, 190, 53, 53, settings);
+
+  // myGLCD.setColor(10, 116, 255);
+  // myGLCD.fillRect (187, 200, 205, 210);
 }
 
 void homeScreen()
 {
-  myGLCD.setColor(VGA_SILVER);
-  myGLCD.fillRect (0, 0, 319, 30);
+  clearMenuArea();
   
-  myGLCD.setBackColor(VGA_SILVER);
-  myGLCD.setColor(VGA_BLACK);
-  myGLCD.setFont(BigFont);
-  myGLCD.print("HOME", 130, 7);
-
-  myGLCD.setColor(VGA_BLACK);
-  myGLCD.fillRect (85, 31, 319, 179);
-
   myGLCD.setColor(VGA_WHITE);
   myGLCD.setBackColor(VGA_BLACK);
   myGLCD.setFont(SevenSegNumFont);
@@ -150,21 +168,16 @@ void homeScreen()
 
   myGLCD.setFont(BigFont);
   myGLCD.print("01.10.2019", 130, 120);
-  
-  myGLCD.drawBitmap(10, 70, 69, 68, home_icon);
 
-  Serial.println("homeScreen done");
-  Serial.println(freeMemory());
+  myGLCD.setColor(VGA_BLACK);
+  myGLCD.fillRect (10, 70, 79, 138);
+  myGLCD.drawBitmap(20, 85, 50, 43, house);
 }
 
 void tempScreen()
 {
-  myGLCD.setColor(VGA_SILVER);
-  myGLCD.fillRect (0, 0, 319, 30);
-  myGLCD.setBackColor(VGA_SILVER);
-  myGLCD.setColor(VGA_BLACK);
-  myGLCD.setFont(BigFont);
-  myGLCD.print("TEMPERATURE", 80, 7);
+  clearMenuArea();
+  
   myGLCD.drawBitmap(10, 70, 69, 68, temp_icon);
 
   myGLCD.setColor(VGA_BLACK);
@@ -175,20 +188,13 @@ void tempScreen()
   myGLCD.setFont(SevenSegNumFont);
   myGLCD.print("22", 130, 55);
   myGLCD.drawBitmap(200, 60, 36, 43, centigrade);
-
-  Serial.println("tempScreen done");
-  Serial.println(freeMemory());
 }
 
 void clockScreen()
 {
-  myGLCD.setColor(VGA_SILVER);
-  myGLCD.fillRect (0, 0, 319, 30);
-  myGLCD.setBackColor(VGA_SILVER);
-  myGLCD.setColor(VGA_BLACK);
-  myGLCD.setFont(BigFont);
-  myGLCD.print("CLOCK", 120, 7);
-  myGLCD.drawBitmap(10, 70, 68, 68, clock_icon);
+  clearMenuArea();
+  
+  myGLCD.drawBitmap(20, 80, 50, 50, clockNeedle);
 
   myGLCD.setColor(VGA_BLACK);
   myGLCD.fillRect (85, 31, 319, 179);
@@ -201,23 +207,13 @@ void clockScreen()
 
   myGLCD.drawBitmap(130, 120, 67, 43, canada);
   myGLCD.print("XX", 210, 120);
-
-  Serial.println("clockScreen done");
-  Serial.println(freeMemory());
 }
 
 void currencyScreen()
 {
-  myGLCD.setColor(VGA_SILVER);
-  myGLCD.fillRect (0, 0, 319, 30);
-  myGLCD.setBackColor(VGA_SILVER);
-  myGLCD.setColor(VGA_BLACK);
-  myGLCD.setFont(BigFont);
-  myGLCD.print("CURRENCY", 100, 7);
-  myGLCD.drawBitmap(10, 70, 69, 68, currency_icon);
-
-  Serial.println("currencyScreen done");
-  Serial.println(freeMemory());
+  clearMenuArea();
+  
+  myGLCD.drawBitmap(28, 75, 35, 60, doller);
 }
 
 // Collection of all screens
@@ -258,9 +254,6 @@ void stateChange(int input)
     Serial.println("??");
     Serial.println(stateActiveIndex);
   }
-
-  Serial.println("stateChange done");
-  Serial.println(freeMemory());
 }
 
 /*************************
@@ -272,7 +265,6 @@ void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   Serial.println("Started");
-  Serial.println(freeMemory());
   
   // Initial setup
   myGLCD.InitLCD();
@@ -286,27 +278,19 @@ void setup() {
 
   myGLCD.setBrightness(16); // default/max is 16
 
+  // Initialize static areas
+  initStaticArea();
+  
   // Start screen
   homeScreen();
 
   // Static screen areas
   rightButton();
+  settingsButton();
   leftButton();
-
-  Serial.println("Setup done");
-  Serial.println(freeMemory());
 }
 
-unsigned int tstLoopStarted = 0;
-
 void loop() {
-
-  if (tstLoopStarted == 0)
-  {
-    Serial.println("Loop started");
-    Serial.println(freeMemory());
-    tstLoopStarted++;
-  }
 
   if (myTouch.dataAvailable())
     {
@@ -316,8 +300,6 @@ void loop() {
 
       if((y_touch > 180) && (y_touch < 230)) // Lower buttons
       {
-        Serial.println("Lower button active");
-        Serial.println(freeMemory());
         
         if((x_touch > 10) && (x_touch < 150)) // Left button
         {
---
+++ statusbar.cpp
@@ -0,0 +1,26 @@
+/*******************************
+ * statusbar.c
+ *
+ * Created: 01.06.2020 21:48:00
+ * Author : DeepSouthT
+ *
+ * Used:
+ *    ToDo
+ * 
+ * Description:
+ *     Status bar displayed at the top of the display
+ *     This includes all its configurations
+ *
+ * Last modified: 04.06.2020
+ *******************************/
+
+ #include "statusbar.h"
+
+ void statusbar::drawStatusbar(void)
+ {
+  statusbarGLCD.setColor(VGA_SILVER);
+  statusbarGLCD.fillRect (0, 0, 319, 30);
+  statusbarGLCD.setBackColor(VGA_SILVER);
+  statusbarGLCD.setColor(VGA_BLACK);
+ }
+ 
